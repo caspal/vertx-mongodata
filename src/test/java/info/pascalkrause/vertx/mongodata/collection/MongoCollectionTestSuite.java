d7a7ce9a0b627f3fe829f637b910cd868c5df48d
Pascal <unknown>
2017-06-27 21:53:32 +0200

    Add service which can be used as datasource for MongoCollection
    Change-Id: Id5c8378ea4c180af103c47a7b2949f35b5ec8f92

--- src/test/java/info/pascalkrause/vertx/mongodata/collection/MongoCollectionTestSuite.java
+++ src/test/java/info/pascalkrause/vertx/mongodata/collection/MongoCollectionTestSuite.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import com.google.common.collect.ImmutableList;
 
@@ -21,70 +22,95 @@ import info.pascalkrause.vertx.mongodata.collection.mongocollection.RemoveTest;
 import info.pascalkrause.vertx.mongodata.collection.mongocollection.UpsertTest;
 import info.pascalkrause.vertx.mongodata.datasource.MongoClientDataSource;
 import info.pascalkrause.vertx.mongodata.datasource.MongoDataSource;
+import info.pascalkrause.vertx.mongodata.service.MongoCollectionServiceImpl;
+import info.pascalkrause.vertx.mongodata.service.MongoCollectionServiceVerticle;
+import info.pascalkrause.vertx.mongodata.service.MongoService;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 
-@RunWith(JUnit4.class)
+@RunWith(VertxUnitRunner.class)
 public class MongoCollectionTestSuite {
-    private static final String SUITE_NAME = "MongoCollection_Test_Suite__";
+    private static final String serviceAddress = "MongoCollectionServiceTest";
+    private static final String SUITE_NAME_CLIENT = "MongoCollection_Test_Suite_Client__";
+    private static final String SUITE_NAME_SERVICE = "MongoCollection_Test_Suite_Service__";
     private static Vertx vertx;
 
     private static JsonObject mongoConfig;
     private static MongodExecutable mongodExecutable;
     private static MongoClient mc;
 
-    public static void main(String[] args) {
-        MongoCollectionTestSuite suite = new MongoCollectionTestSuite();
-        try {
-            suite.run();
-        } catch (IOException e) {
-        } finally {
-            suite.tearDown();
-        }
-    }
-
     private void startMongoDB() throws IOException {
-        mongoConfig = TestUtils.getDefaultMongoConfig();
         mongodExecutable = TestUtils.prepareMongo(mongoConfig);
         mongodExecutable.start();
         System.out.println(new StringBuilder("Embedded MongoDB Started with:\n")
                 .append(Json.encodePrettily(mongoConfig)).toString());
     }
 
-    private void tearUp() throws IOException {
+    @Before
+    public void tearUp() throws IOException {
+        mongoConfig = TestUtils.getDefaultMongoConfig();
         startMongoDB();
         vertx = Vertx.vertx();
         mc = MongoClient.createShared(vertx, mongoConfig);
     }
 
-    private void tearDown() {
+    @After
+    public void tearDown(TestContext context) {
         mongodExecutable.stop();
-        vertx.close();
+        vertx.close(context.asyncAssertSuccess());
     }
 
-    private List<AbstractMongoCollectionTest> initializeTestSuites(MongoDataSource mds) {
+    private List<AbstractMongoCollectionTest> initializeSuites(MongoDataSource mds, String suiteName) {
         List<AbstractMongoCollectionTest> suites = ImmutableList.<AbstractMongoCollectionTest>builder()
-                .add(new Find_FindAllTest(SUITE_NAME, mds, mc))
-                .add(new UpsertTest(SUITE_NAME, mds, mc))
-                .add(new RemoveTest(SUITE_NAME, mds, mc))
-                .add(new BulkInsertTest(SUITE_NAME, mds, mc))
-                .add(new DropTest(SUITE_NAME, mds, mc))
-                .add(new Count_CountAllTest(SUITE_NAME, mds, mc)).build();
+                .add(new Find_FindAllTest(suiteName, mds, mc)).add(new UpsertTest(suiteName, mds, mc))
+                .add(new RemoveTest(suiteName, mds, mc)).add(new BulkInsertTest(suiteName, mds, mc))
+                .add(new DropTest(suiteName, mds, mc)).add(new Count_CountAllTest(suiteName, mds, mc)).build();
         return suites;
     }
 
     @Test
-    public void run() throws IOException {
-        tearUp();
-        MongoDataSource mds = new MongoClientDataSource(mc);
-        List<AbstractMongoCollectionTest> clientSuites = initializeTestSuites(mds);
-
-        clientSuites.forEach(suite -> {
-            suite.getSuite().run(vertx, TestUtils.getTestOptions(vertx)).awaitSuccess(TimeUnit.SECONDS.toMillis(10));
+    public void testClientDatasource(TestContext testContext) throws IOException {
+        Async testComplete = testContext.async();
+        // Run Client Tests
+        initializeSuites(new MongoClientDataSource(mc), SUITE_NAME_CLIENT).forEach(suite -> {
+            suite.getSuite().run(vertx, TestUtils.getTestOptions(vertx)).awaitSuccess(TimeUnit.SECONDS.toMillis(2));
         });
-        // Run after all Suits are completed
-        tearDown();
+        testComplete.complete();
+    }
+
+    @Test
+    public void testServiceDatasource(TestContext testContext) throws IOException {
+        Async testComplete = testContext.async();
+        vertx.deployVerticle(new MongoCollectionServiceVerticle(serviceAddress, mongoConfig), deployResponse -> {
+            if (deployResponse.failed()) {
+                return;
+            }
+            MongoService mcs = new MongoCollectionServiceImpl(vertx.eventBus(), serviceAddress);
+            mcs.checkAvailability(availableCheck -> {
+                if (availableCheck.failed()) {
+                    return;
+                }
+                List<AbstractMongoCollectionTest> suites = initializeSuites(mcs.getDataSource(), SUITE_NAME_SERVICE);
+                vertx.executeBlocking(fut -> {
+                    suites.forEach(suite -> {
+                        try {
+                            suite.getSuite().run(vertx, TestUtils.getTestOptions(vertx))
+                                    .awaitSuccess(TimeUnit.SECONDS.toMillis(2));
+                        } catch (Throwable t) {
+                            testContext.fail(t);
+                            testComplete.complete();
+                        }
+                    });
+                    fut.complete();
+                }, res -> {
+                    testComplete.complete();
+                });
+            });
+        });
     }
 }
